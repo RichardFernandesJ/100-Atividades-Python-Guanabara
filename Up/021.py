5801a35b5d8b9c4a03234d1dced5f34ce9e676ae
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-03-26 15:09:32 -0300

    commit

---
+++ Up/021.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('iknow.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
+
+
+
+#Reprodutor de musica mp3 utilizando o pygame conforme o curso foi necessario incluir o ("input()) para que reproduza o som graças a ajuda dos comentarios no video do curso deu tudo certo.
