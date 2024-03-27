9529647d833685ad590e55d0c494415b45d79318
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-03-26 22:14:02 -0300

    commit

--- Up/023.py
+++ Up/023.py
@@ -1,9 +1,18 @@
 num = int(input('Digite um número: '))
-numb = str(num)
-print('O numero digitado foi {}'.format(num))
-print('Unidade {}'.format(numb[3]))
-print('Dezena {}'.format(numb[2]))
-print('Centena {}'.format(numb[1]))
-print('Milhar {}'.format(numb[0]))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando o numero {}'.format(num))
+print('Unidade {}'.format(u))
+print('Dezena {}'.format(d))
+print('Centena {}'.format(c))
+print('Milhar {}'.format(m))
+#numb = str(num)
+#print('O numero digitado foi {}'.format(num))
+#print('Unidade {}'.format(numb[3]))
+#print('Dezena {}'.format(numb[2]))
+#print('Centena {}'.format(numb[1]))
+#print('Milhar {}'.format(numb[0]))
 
 
