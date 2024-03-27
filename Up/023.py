531eacfdfe1dc10dd9a3ff03a93d597dc12933b3
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-03-26 22:08:39 -0300

    Update 023.py

---
+++ Up/023.py
@@ -0,0 +1,9 @@
+num = int(input('Digite um número: '))
+numb = str(num)
+print('O numero digitado foi {}'.format(num))
+print('Unidade {}'.format(numb[3]))
+print('Dezena {}'.format(numb[2]))
+print('Centena {}'.format(numb[1]))
+print('Milhar {}'.format(numb[0]))
+
+
