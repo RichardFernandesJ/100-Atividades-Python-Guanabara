8ee235453d02e922f121d4c4a2b20b99296ba0af
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-03-28 14:37:06 -0300

    commit

--- Up/028.py
+++ Up/028.py
@@ -1,10 +1,15 @@
 import random
-print('========BLAZE==========')
+collor = {'amarelo':'\033[33m',
+          'vermelho':'\033[31m',
+          'clear':'\033[m',
+          'negrito':'\033[1m'}
+
+print('\033[31m''========BLAZE==========''\033[m')
 print('Bip..Bop...Bip..')
 print('Estou pensando em um numero !')
 print('Você acha que consegue advinhar qual ?')
 print('Bip....°°')
-print('========BLAZE==========')
+print('\033[31m''========BLAZE==========''\033[m')
 resp = str(input('Sim ou Não: ')).strip().upper()
 if resp == 'SIM':
     print('Então Vamos lá!')
