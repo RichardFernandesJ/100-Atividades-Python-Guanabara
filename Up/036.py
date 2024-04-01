a096404de0f1a0408ead2e0130b7470a5bcd2894
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-03-31 22:52:02 -0300

    Update 036.py

--- Up/036.py
+++ Up/036.py
@@ -1,3 +1,6 @@
 print('=-'*10)
 print('EMPRÉSTIMO BANCÁRIO')
 print('=-'*10)
+val = int(input('Informe o valor da Casa: '))
+sal = int(input('Informe o salário do comprador: '))
+anos = int(input('Informe em quantos anos deseja pagar: '))
