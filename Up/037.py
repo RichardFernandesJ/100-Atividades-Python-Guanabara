3d2f53fe64acd88dc64f7f9d67b82df79758af44
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-04-04 16:47:37 -0300

    Update 037.py

---
+++ Up/037.py
@@ -0,0 +1,17 @@
+print('=-'*10)
+print('BASE CONVERTER 1.0')
+print('=-'*10)
+number = int(input('Digite um número inteiro: ').strip())
+print('|Escolha uma das bases para conversão|')
+print('[ 1 ] Converter para BINÁRIO')
+print('[ 2 ] Converter para OCTAL')
+print('[ 3 ] Converter para HEXADECIMAL')
+op = int(input('Sua opção: ').strip())
+if op == 1:
+    print('{} convertido para BINÁRIO é igual a {}'.format(number, bin(number)))
+elif op == 2:
+    print('{} convertido para OCTAL é igual a {}'.format(number, oct(number)))
+elif op == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(number, hex(number)))
+else:
+    print('ESTA OPÇÃO É INVALIDA. TENTE NOVAMENTE.')
