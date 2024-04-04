bda9c5b5afc52cd876cc27b54d7b3c4cbf7f936b
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-04-04 17:22:07 -0300

    Update 039.py

--- Up/039.py
+++ Up/039.py
@@ -5,10 +5,14 @@ print('-'*10)
 ano = int(input('Informe seu ano de nascimento: ').strip())
 anoa = datetime.now().year
 id = anoa - ano
-print(id)
+temp = 18 - id
+tempp = id - 18
 if id == 18:
     print('Esta na hora de se alistar !')
+    print('Realize o Alistamento através do nosso site http://juntamilitarmg.gov')
 elif id < 18:
     print('Esta quase na hora de se alistar no SERVIÇO MILITAR !')
+    print('Falta {} anos para se Alistar!'.format(temp))
 elif id > 18:
     print('Passou do tempo de alistamento !')
+    print('Ja se passou {} anos para seu alistamento !'.format(tempp))
