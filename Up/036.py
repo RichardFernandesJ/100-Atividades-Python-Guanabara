9f70b1036e03ef7f8e2d02da1df5442c068b44e6
Richard Fernandes de Jesus <richard.tec1999@gmail.com>
2024-04-04 12:52:24 -0300

    Update 036.py

--- Up/036.py
+++ Up/036.py
@@ -1,13 +1,14 @@
 print('=-'*10)
 print('EMPRÉSTIMO BANCÁRIO')
 print('=-'*10)
-val = int(input('Informe o valor da Casa: R$').strip())
-sal = int(input('Informe o salário do comprador: R$').strip())
+val = float(input('Informe o valor da Casa: R$').strip())
+sal = float(input('Informe o salário do comprador: R$').strip())
 anos = int(input('Quantos anos de Financiamento? ').strip())
 tot = (sal * 30) / 100
 mes = (12 * anos)
 toth = val / mes
-print('Para pagar uma casa de R${} em {} anos a prestação será de R${:.2f}!'.format(val, anos, toth))
+print('Para pagar uma casa de R${:.2f} em {} anos a prestação será de R${:.2f}'.format(val, anos, toth), end='')
+print('!')
 if toth > tot:
     print('Empréstimo NEGADO!')
 else:
